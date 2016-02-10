75373a85ef58ef84a87c5d13db785d6b75288a2f
jjl014 <jimmyli921@gmail.com>
2016-02-10 08:07:49 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,19 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/"+idNumber, addProject);
+	console.log("project/" + idNumber);
+}
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML ='<img src="' + result['image'] + '" class="detailsImage">' 
+	+ '<p><small>' + result['date'] + '</small>' + result['summary'] + '</p>';
+
+	 var query = "#project" + result['id'] + " .details";
+
+	 $(query).html(projectHTML);
+
 }
 
 /*
@@ -35,4 +48,18 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	e.preventDefault();
+	$.get("/palette", randomPalette);
 }
+
+function randomPalette(result){
+	//console.log(result);
+	var colors = result['colors'];
+	colors = colors['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
